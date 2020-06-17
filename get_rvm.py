fixed formula and save to file option

get_rvm.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
 
     fig = plt.figure()
     ax1 = fig.add_subplot(1, 1, 1)
-    phase = []
+    #phase = []
+    e_m = []
 
     for filename in args.filenames:
         domain_radius = h5py.File(filename, 'r')['run_config']['domain_radius'][()]
@@ -33,6 +34,8 @@ if __name__ == "__main__":
         phi   = loaders.get_dataset(filename, 'phi')
         sigma = loaders.get_dataset(filename, 'sigma')
 
+        v_kep = np.sqrt(1./r)
+
 
 
         #ECCENTRICITY CALCULATION MM08
@@ -40,34 +43,17 @@ if __name__ == "__main__":
         radial_cut = (r > 1.5) * (r < 5.0)
 
         m = 1
-        top = np.sum((dA * sigma * vr * np.exp(1.j * m * phi)) * radial_cut)
-        bottom = np.sum((dA * sigma * vp) * radial_cut)
-        e_m = np.absolute(top/bottom)
-        print e_m
-        phase.append(np.angle(top/bottom))
-       
-
-        # ax1.hist(
-        #     vx.flat,
-        #     weights=(dA * (r < 3) * (r > 2)).flat,
-        #     bins=100,
-        #     density=True,
-        #     histtype='step',
-        #     label=r'$\rm{{orbit}} = {:.01f}$'.format(time / 2 / np.pi))
+        top = 2. * np.sum((dA * sigma * vr * np.exp(1.j * m * phi)) * radial_cut)
+        bottom = np.sum((dA * sigma * v_kep) * radial_cut)
+        e_m.append(np.absolute(top/bottom))
+        #print e_m
+        #phase.append(np.angle(top/bottom))
     
-    dphi = np.diff(phase)
-    dphi[dphi < -0.9 * 2 * np.pi] += 2 * np.pi
-    dphi[dphi > +0.9 * 2 * np.pi] -= 2 * np.pi
-    cumphase = np.cumsum(dphi)
-
-    #for i in range(len(cumphase)):
-        #print cumphase[i]
+    np.savetxt(e_m,'testdata.txt')
 
     
+    # dphi = np.diff(phase)
+    # dphi[dphi < -0.9 * 2 * np.pi] += 2 * np.pi
+    # dphi[dphi > +0.9 * 2 * np.pi] -= 2 * np.pi
+    # cumphase = np.cumsum(dphi)
 
-    #ax1.set_xlim(-0.2, 1.2)
-    #ax1.set_xlabel(r'$e^2$')
-    #ax1.set_ylabel(r'$P(e^2)$')
-    #ax1.set_yscale('log')
-    #ax1.legend()
-    #plt.show()