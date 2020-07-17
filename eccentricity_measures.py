169544c704cde551154483abc9309fe91448a13d
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-07-17 18:36:30 -0400

    Update eccentricity_measures.py

--- eccentricity_measures.py
+++ eccentricity_measures.py
@@ -23,12 +23,12 @@ def gas_eccentricity_diagnostics(filename):
     domain_radius = h5py.File(filename, 'r')['run_config']['domain_radius'][()]
     rbins = np.linspace(0.0, domain_radius, 256)
 
-    GM    = 1.0
-    E     = +0.5 * (vp**2 + vr**2) - GM / r
-    a     = -0.5 * GM / E
-    omega2 = (GM / a**3)
-    L     = r * vp
-    e2    = 1.0 - (0.5 * L / E)**2 * omega2
+    GM     = 1.0
+    E      = +0.5 * (vp**2 + vr**2) - GM / r
+    a      = -0.5 * GM / E
+    omega2 = GM / a**3
+    L      = r * vp
+    e2     = 1.0 - (0.5 * L / E)**2 * omega2
 
     e2dM_binned, _ = np.histogram(r, weights=dM * e2, bins=rbins)
     dM_binned,   _ = np.histogram(r, weights=dM,      bins=rbins)
@@ -37,19 +37,42 @@ def gas_eccentricity_diagnostics(filename):
     vrm1dM, _ = np.histogram(r, weights=dM * vr * np.exp(1.j * phi), bins=rbins)
     vpdM,   _ = np.histogram(r, weights=dM * vp,                     bins=rbins)
 
-    kd06_vs_r  = (e2dM_binned / dM_binned)**0.5
-    mm08_vs_r  = vrm1dM / vpdM
-    sigma_vs_r = dM_binned/dA_binned
+    # For each checkpoint, we need to generate a file containing these measurements:
+    # 1. Surface density radial profile
+    # 2. MM08 as a function of r
+    # 3. KD06 as a function of r
+    # 4. MM08 globally
+    # 5. KD06 globally
+    # 6. Phase angle as a function of r
+    # 7. Phase angle globally
+    # 8. Filename (diagnostics file that was analyzed, and the time)
 
-    mm08_e_vs_r = np.absolute(mm08_vs_r)
-    mm08_phase_vs_r = np.angle(mm08_vs_r)
-    
-    plt.figure()
-    plt.plot(rbins[:-1],kd06_vs_r,'-o',label='KD06')
-    plt.plot(rbins[:-1],mm08_e_vs_r,'-o',label='MM08')
-    plt.legend()
-    plt.show()
+    radial_cut = (rbins[1:] > 1.5) * (rbins[1:] < 5.0)
+    sigma_vs_r = dM_binned / dA_binned
 
+    # MM08 measurements:
+    mm08_vs_r         = vrm1dM / vpdM
+    mm08_global       = vrm1dM[radial_cut].sum() / vpdM[radial_cut].sum()
+    mm08_e_vs_r       = np.absolute(mm08_vs_r)
+    mm08_phase_vs_r   = np.angle(mm08_vs_r)
+    mm08_e_global     = np.absolute(mm08_global)
+    mm08_phase_global = np.angle(mm08_global)
+
+    # KD06 measurements:
+    kd06_e_vs_r   = (e2dM_binned / dM_binned)**0.5
+    kd06_e_global = (e2dM_binned[radial_cut].sum() / dM_binned[radial_cut].sum())**0.5
+
+    h5f = h5py.File(filename.replace('.h5', '.eccentricity_measures.h5'), 'w')
+    h5f['sigma_vs_r']        = sigma_vs_r
+    h5f['mm08_vs_r']         = mm08_vs_r
+    h5f['mm08_e_vs_r']       = mm08_e_vs_r
+    h5f['mm08_phase_vs_r']   = mm08_phase_vs_r
+    h5f['mm08_e_global']     = mm08_e_global
+    h5f['mm08_phase_global'] = mm08_phase_global
+    h5f['kd06_e_vs_r']       = kd06_e_vs_r
+    h5f['kd06_e_global']     = kd06_e_global
+    h5f['time']              = h5py.File(filename, 'r')['time'][()]
+    h5f['filename']          = filename
 
 
 
