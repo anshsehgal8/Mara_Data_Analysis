7fab037179445298a65c108bd5f9dd3af17c4965
Ansh Sehgal <anshsehgal8@gmail.com>
2020-07-17 17:58:05 -0400

    7/17 update to eccentricity measures

--- eccentricity_measures.py
+++ eccentricity_measures.py
@@ -5,6 +5,7 @@ import pickle
 import numpy as np
 import h5py
 import loaders
+import matplotlib.pyplot as plt
 
 
 
@@ -25,18 +26,30 @@ def gas_eccentricity_diagnostics(filename):
     GM    = 1.0
     E     = +0.5 * (vp**2 + vr**2) - GM / r
     a     = -0.5 * GM / E
-    omega = (GM / a**3)**0.5
+    omega2 = (GM / a**3)
     L     = r * vp
-    e2    = 1.0 - (0.5 * omega * L / E)**2
+    e2    = 1.0 - (0.5 * L / E)**2 * omega2
 
     e2dM_binned, _ = np.histogram(r, weights=dM * e2, bins=rbins)
     dM_binned,   _ = np.histogram(r, weights=dM,      bins=rbins)
+    dA_binned,   _ = np.histogram(r, weights=dA,      bins=rbins)
 
     vrm1dM, _ = np.histogram(r, weights=dM * vr * np.exp(1.j * phi), bins=rbins)
     vpdM,   _ = np.histogram(r, weights=dM * vp,                     bins=rbins)
 
-    kd06_vs_r = (e2dM_binned / dM_binned)**0.5
-    mm08_vs_r = vrm1dM / vpdM
+    kd06_vs_r  = (e2dM_binned / dM_binned)**0.5
+    mm08_vs_r  = vrm1dM / vpdM
+    sigma_vs_r = dM_binned/dA_binned
+
+    mm08_e_vs_r = np.absolute(mm08_vs_r)
+    mm08_phase_vs_r = np.angle(mm08_vs_r)
+    
+    plt.figure()
+    plt.plot(rbins[:-1],kd06_vs_r,'-o',label='KD06')
+    plt.plot(rbins[:-1],mm08_e_vs_r,'-o',label='MM08')
+    plt.legend()
+    plt.show()
+
 
 
 
